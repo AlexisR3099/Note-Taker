9c972ba748a3b6b6294a624ae5b2c2550e0a1731
Slimeball <alexramo2330@gmail.com>
2022-08-16 21:49:52 -0500

    refractor-post-route-working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,33 @@
 // middleware function
 const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/notesRoutes');
+const { text } = require('express');
+const fs = require('fs');
+const { title } = require('process');
+const { v4: uuidv4 } = require('uuid');
 
-router.use(noteRoutes);
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if(err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+router.post('/notes', (req,res) => {
+    const newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+        });
+    });
+    res.json(newNote);
+});
 
 module.exports = router;
