af56d1d9b229722ddb22e16be729967ab57c7a8a
Slimeball <alexramo2330@gmail.com>
2022-08-16 20:02:33 -0500

    get-routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,4 +1,13 @@
 // middleware 
+const path = require('path');
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+})
 
 module.exports = router;
